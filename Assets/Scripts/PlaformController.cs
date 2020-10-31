b92e1689c6dd5d62c48dd943b6642055581ba669
winchell459 <johncmorris@gmail.com>
2020-10-31 11:01:17 -0700

    end of day 10/31/20

---
+++ Assets/Scripts/PlaformController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaformController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float speed = 4; //float 9.0 or 6.7 ... decimal number
+    float jumpForce = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+        
+    }
+
+    void Jump()
+    {
+        bool jumpButtonDown = Input.GetKeyDown(KeyCode.Space);
+        if (jumpButtonDown)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
